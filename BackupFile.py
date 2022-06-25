58bdd9558749008acf8f80f03ebf2c744cec8f15
Gayathri5151 <73778830+Gayathri5151@users.noreply.github.com>
2022-06-25 14:55:45 +0530

    Add files via upload

---
+++ BackupFile.py
@@ -0,0 +1,9 @@
+import os
+import shutil
+source = input('Enter the source folder name: ')
+destination = input('Enter Destination folder: ')
+source = source+ '/'
+destination= destination+'/'
+listoffiles= os.listdir(source)
+for file in listoffiles:
+    shutil.move((source+file), destination)
